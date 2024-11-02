4636670f3b8bd506cac8706672d5559961549179
Jason Masten <jamasten@microsoft.com>
2024-11-02 10:12:20 -0400

    Further simplified, Added comments, Changed data type

--- Scripts/Remove-BuiltinApps.ps1
+++ Scripts/Remove-BuiltinApps.ps1
@@ -1,37 +1,76 @@
-﻿$apps = "Microsoft.3DBuilder", "Microsoft.BingWeather", "Microsoft.BingNews", "# Microsoft.DesktopAppInstaller", "Microsoft.GamingApp",`
-    "Microsoft.WindowsFeedbackHub", "Microsoft.GetHelp", "Microsoft.Getstarted", "Microsoft.HEIFImageExtension", `
-    "Microsoft.Messaging", "Microsoft.Microsoft3DViewer", "Microsoft.MicrosoftOfficeHub", `
-    "Microsoft.MicrosoftSolitaireCollection", "Microsoft.MicrosoftStickyNotes", `
-    "Microsoft.MixedReality.Portal", "Microsoft.MSPaint", "Microsoft.Office.OneNote", `
-    "Microsoft.OneConnect", "Microsoft.Outlook.DesktopIntegrationServices", "Microsoft.OutlookForWindows", `
-    "Microsoft.Paint", "Microsoft.People", "Microsoft.Print3D", "Microsoft.ScreenSketch", `
-    "Microsoft.SkypeApp", "# Microsoft.StorePurchaseApp", "Microsoft.VP9VideoExtensions", `
-    "Microsoft.Wallet", "Microsoft.WebMediaExtensions", "Microsoft.WebpImageExtension", "Microsoft.Windows.Photos", `
-    "Microsoft.WindowsAlarms", "Microsoft.WindowsCalculator", "Microsoft.WindowsCamera", `
-    "microsoft.windowscommunicationsapps", "Microsoft.WindowsFeedbackHub", "Microsoft.WindowsMaps", `
-    "Microsoft.WindowsSoundRecorder", "# Microsoft.WindowsStore", "Microsoft.Xbox.TCUI", "Microsoft.XboxApp", `
-    "Microsoft.XboxGameOverlay", "Microsoft.XboxGamingOverlay", "Microsoft.XboxIdentityProvider", `
-    "Microsoft.XboxSpeechToTextOverlay", "Microsoft.YourPhone", "Microsoft.ZuneMusic", "Microsoft.ZuneVideo", `
-    "MicrosoftCorporationII.QuickAssist", "Microsoft.Todos", "Clipchamp.Clipchamp", "Microsoft.Whiteboard", "Microsoft.PowerAutomateDesktop", `
-    "Microsoft.Windows.DevHome"
-
-$applist = $apps | Where-Object {$_ -inotlike '#*'}
+﻿# Remove Windows Default Applications
+$WindowsApplications = @(
+    #"Microsoft.DesktopAppInstaller",
+    #"Microsoft.StorePurchaseApp",
+    #"Microsoft.WindowsStore",
+    "Clipchamp.Clipchamp",
+    "Microsoft.3DBuilder",
+    "Microsoft.BingNews",
+    "Microsoft.BingWeather",
+    "Microsoft.GamingApp",
+    "Microsoft.GetHelp",
+    "Microsoft.Getstarted",
+    "Microsoft.HEIFImageExtension",
+    "Microsoft.Messaging",
+    "Microsoft.Microsoft3DViewer",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.MicrosoftStickyNotes",
+    "Microsoft.MixedReality.Portal",
+    "Microsoft.MSPaint",
+    "Microsoft.Office.OneNote",
+    "Microsoft.OneConnect",
+    "Microsoft.Outlook.DesktopIntegrationServices",
+    "Microsoft.OutlookForWindows",
+    "Microsoft.Paint",
+    "Microsoft.People",
+    "Microsoft.PowerAutomateDesktop",
+    "Microsoft.Print3D",
+    "Microsoft.ScreenSketch",
+    "Microsoft.SkypeApp",
+    "Microsoft.Todos",
+    "Microsoft.VP9VideoExtensions",
+    "Microsoft.Wallet",
+    "Microsoft.WebMediaExtensions",
+    "Microsoft.WebpImageExtension",
+    "Microsoft.Whiteboard",
+    "Microsoft.Windows.DevHome",
+    "Microsoft.Windows.Photos",
+    "Microsoft.WindowsAlarms",
+    "Microsoft.WindowsCalculator",
+    "Microsoft.WindowsCamera",
+    "microsoft.windowscommunicationsapps",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsMaps",
+    "Microsoft.WindowsSoundRecorder",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxApp",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo",
+    "MicrosoftCorporationII.QuickAssist"
+)
 $ProvisionedApps = Get-AppxProvisionedPackage -online
 $InstalledApps = Get-AppxPackage -AllUsers
 
-ForEach ($app in $applist) {
+ForEach ($Application in $WindowsApplications) {
 
-    If ($($ProvisionedApps.DisplayName) -contains $app) {
-        Get-AppxProvisionedPackage -online | Where-Object {$_.DisplayName -eq "$app"} | Remove-AppxProvisionedPackage -online
+    If ($($ProvisionedApps.DisplayName) -contains $Application) {
+        Get-AppxProvisionedPackage -online | Where-Object {$_.DisplayName -eq "$Application"} | Remove-AppxProvisionedPackage -online
     }
 
-    If ($($InstalledApps.Name) -contains $app) {
-        Get-AppxPackage -AllUsers | Where-Object { $_.Name -eq "$app" } | Remove-AppxPackage -AllUsers
+    If ($($InstalledApps.Name) -contains $Application) {
+        Get-AppxPackage -AllUsers | Where-Object { $_.Name -eq "$Application" } | Remove-AppxPackage -AllUsers
     }
-
 }
-$capabilitylist = "App.Support.ContactSupport", "App.Support.QuickAssist"
 
+# Remove Support Apps
+$capabilitylist = "App.Support.ContactSupport", "App.Support.QuickAssist"
 ForEach ($capability in $capabilitylist) {
     $InstalledCapability = $null
     $InstalledCapability = Get-WindowsCapability -Online | Where-Object { $_.Name -like "$capability*" -and $_.State -ne "NotPresent" }
